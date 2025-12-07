324f48cf7fd6bcc308ea341cefdc6f66f6151642
githubPhuc <0306191061@caothang.edu.vn>
2025-12-07 12:44:38 +0700

    upp

--- resources/views/dashboard/categories/service/index.blade.php
+++ resources/views/dashboard/categories/service/index.blade.php
@@ -143,10 +143,10 @@
                             @endphp
                             <tr class="hover:bg-blue-50 transition-colors duration-150">
                                <td class="px-6 py-4">
-    <img src="{{ src_img_get($category->image) }}"
-         alt="{{ $category->category_name }}"
-         class="w-12 h-12 rounded-lg object-cover ring-1 ring-gray-200 bg-gray-50">
-</td>
+                                    <img src="{{ src_img_get($category->image) }}"
+                                        alt="{{ $category->category_name }}"
+                                        class="w-12 h-12 rounded-lg object-cover ring-1 ring-gray-200 bg-gray-50">
+                                </td>
 
                                 <td class="px-6 py-4">
                                     <div class="flex items-center space-x-3">
